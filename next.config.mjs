fa64e16eec787519fc8b830625736e2ae6e8648a
lumpen <swk5373@gmail.com>
2024-10-10 14:00:02 +0900

    📦[Chore] config - svg 최적화 허용 #9

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,9 @@ const nextConfig = {
         hostname: '**',
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
