2aa9c799430639d51fecdf700019aaf7d17fb95b
cchaeyoung <kimchaeyoung05@naver.com>
2024-10-14 00:04:29 +0900

    ♻️[Refactor] 캘린더 페이지 파일 통합 #10

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,5 +1,17 @@
-import CalendarBlock from "./calendar-block";
+"use client";
+
+import CalendarHeader from "./components/calendar-header";
+import ScheduleList from "./components/schedule-list";
+import StyleSetting from "./components/style-setting";
 
 export default function CalendarPage() {
-  return <CalendarBlock />;
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <CalendarHeader />
+        <StyleSetting />
+        <ScheduleList />
+      </div>
+    </>
+  );
 }
