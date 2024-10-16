4329fc18676fc80be46a432e2f83d7ec4286d6ba
an seungji <ansj8777@gmail.com>
2024-10-16 14:01:12 +0900

    📦[Chore] github action 실행 중 에러 발생, build 실행을 통핸 테스트 진행, fix 커맨드 추가 #5

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cd ./  # 프로젝트의 올바른 경로로 이동
-
-# 빌드 결과물을 output 디렉토리로 복사
+cd ./
 mkdir output
-cp -R .next/* ./output
+cp -R ./linkle/* ./output
+cp -R ./output ./linkle/
--- src/app/profile/[id]/detail/page.tsx
+++ src/app/profile/[id]/detail/page.tsx
@@ -1,7 +1,7 @@
 export default function ProfileDetail() {
-    return (
-        <div>
-            <h1>유저 프로필</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>유저 프로필</h1>
+    </div>
+  );
 }
