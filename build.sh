371b73507d061a564f196c9d7e8cc89f36cddca3
an seungji <ansj8777@gmail.com>
2024-10-16 15:53:02 +0900

    📦[Chore] 현재 디렉토리의 복사 과정에서의 자기복사 문제 해결 #5

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+# Next.js 빌드 실행
+npm run build
+
+# 기존 output 디렉토리 삭제 (있을 경우)
+rm -rf output
+
+# output 디렉토리 생성
 mkdir output
-cp -R ./* ./output/
+
+# 현재 디렉토리의 모든 내용(자신 제외)을 output 디렉토리로 복사
+rsync -av --progress ./ ./output --exclude output
