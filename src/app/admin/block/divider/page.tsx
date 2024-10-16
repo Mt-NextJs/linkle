c2cc682e7e7fd9b76b8a0e18ecb8a70253760909
cchaeyoung <kimchaeyoung05@naver.com>
2024-10-16 14:04:06 +0900

    ✨[Feat] 구분선 페이지에 컴포넌트 통합 #36

--- src/app/admin/block/divider/page.tsx
+++ src/app/admin/block/divider/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { useState } from "react";
 import Layout from "../components/layout";
+import DividerPreview from "./components/divider-preview";
+import DividerSelector from "./components/divider-selector";
+import { DividerType } from "./types";
 
 export default function DividerPage() {
+  const [selectedDivider, setSelectedDivider] = useState<DividerType>("공백");
+
   return (
     <Layout title="구분선 블록">
-      <div></div>
+      <DividerPreview selectedDivider={selectedDivider} />
+      <DividerSelector
+        onSelect={setSelectedDivider}
+        selected={selectedDivider}
+      />
     </Layout>
   );
 }
