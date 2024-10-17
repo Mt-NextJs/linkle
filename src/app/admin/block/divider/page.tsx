b005ed246c706a675556347564ef70734fca0d0d
cchaeyoung <kimchaeyoung05@naver.com>
2024-10-17 11:26:12 +0900

    ✨[Feat] 구분선 블록에 버튼 컴포넌트 추가 #36

--- src/app/admin/block/divider/page.tsx
+++ src/app/admin/block/divider/page.tsx
@@ -5,9 +5,15 @@ import Layout from "../components/layout";
 import DividerPreview from "./components/divider-preview";
 import DividerSelector from "./components/divider-selector";
 import { DividerType } from "./types";
+import ButtonBox from "../components/buttons/button-box";
+import AddButton from "../components/buttons/add-button";
 
 export default function DividerPage() {
   const [selectedDivider, setSelectedDivider] = useState<DividerType>("공백");
+
+  const handleAddButtonClick = () => {
+    console.log(`선택된 구분선: ${selectedDivider}`);
+  };
 
   return (
     <Layout title="구분선 블록">
@@ -16,6 +22,9 @@ export default function DividerPage() {
         onSelect={setSelectedDivider}
         selected={selectedDivider}
       />
+      <ButtonBox>
+        <AddButton text="추가 완료" onClick={handleAddButtonClick} />
+      </ButtonBox>
     </Layout>
   );
 }
