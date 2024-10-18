0237623170fb106949e011399413b85386d6f06e
wynter24 <m.wynter24.k@gmail.com>
2024-10-18 13:44:35 +0900

    💄[Style] Prettier 포맷팅 수정 및 .gitattributes 추가하여 LF로 줄바꿈 고정 #8

--- src/lib/get-sequence.ts
+++ src/lib/get-sequence.ts
@@ -20,7 +20,7 @@ export async function getSequence(token: string) {
     const result = await response.json();
     if (result.code === 200) {
       const blockList = result.data;
-      if(blockList.length === 0) return 1;
+      if (blockList.length === 0) return 1;
       else return blockList[blockList.length - 1].sequence;
     }
   } catch (error) {
