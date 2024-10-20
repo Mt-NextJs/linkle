5c2ff3eb9c6f8b988556f7fa2ef0c981e895ed1d
an seungji <ansj8777@gmail.com>
2024-10-20 16:41:18 +0900

    ✨[Feat] 중간 병합 후 배포 테스트를 위한 커밋 #91

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token"); // 쿠키에서 인증 토큰을 가져옴
+  // const token = request.cookies.get("token"); // 쿠키에서 인증 토큰을 가져옴
 
-  if (
-    !token &&
-    request.nextUrl.pathname !== "/login" &&
-    request.nextUrl.pathname !== "/landing"
-  ) {
-    return NextResponse.redirect(new URL("/landing", request.url));
-  }
+  // if (
+  //   !token &&
+  //   request.nextUrl.pathname !== "/login" &&
+  //   request.nextUrl.pathname !== "/landing"
+  // ) {
+  //   return NextResponse.redirect(new URL("/landing", request.url));
+  // }
 
   return NextResponse.next(); // 로그인된 사용자에 대한 요청은 통과
 }
