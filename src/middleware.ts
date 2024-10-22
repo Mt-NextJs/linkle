e1976cdbf9853a096f5c30f9ca039cd46226f5d9
an seungji <ansj8777@gmail.com>
2024-10-22 11:19:56 +0900

    ✨[Feat] 이미 로그인되어 있는 사용자에 대한 로그인, 회원가입, 인트로 페이지 접근 막기 : 미들웨어를 통한 구현 #32

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,10 +12,19 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/intro", request.url));
   }
 
+  // 이미 로그인된 사용자가 로그인 또는 회원가입 페이지에 접근하면 메인 페이지로 리다이렉트
+  if (
+    token &&
+    (request.nextUrl.pathname === "/login" ||
+      request.nextUrl.pathname === "/join")
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
   return NextResponse.next();
 }
 
 // 인증이 필요한 페이지 설정
 export const config = {
-  matcher: ["/", "/profile/:path*", "/admin/:path*"], // 인증이 필요한 경로 지정
+  matcher: ["/", "/profile/:path*", "/admin/:path*", "/login", "/join"], // 인증이 필요한 경로 지정
 };
