d514974ed9608c642e7adb0ae8b3d19b39f1ed4b
an seungji <ansj8777@gmail.com>
2024-10-22 15:43:03 +0900

    💄[Design] 랜딩페이지 스타일링 및 퍼블리싱 진행 #32

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -3,13 +3,28 @@ import { ClientRoute } from "@config/route";
 
 export default function Intro() {
   return (
-    <div className={"flex w-full flex-col items-center py-10"}>
-      <Link href={ClientRoute.LOGIN as string} className={"button color"}>
-        시작하기
-      </Link>
-      <Link href={ClientRoute.ADMIN as string} className={"button color"}>
-        관리자
-      </Link>
+    <div className="h-screen bg-primary p-40">
+      {/* 스크린 리더 전용 텍스트*/}
+      <h1 className="sr-only">
+        Link At Once! 소셜 프로필 링크 관리 서비스 in my link 입니다!
+      </h1>
+      <p className="sr-only">시작하기 버튼을 눌러 로그인을 진행해 주세요.</p>
+      <div className="flex h-full w-full flex-col justify-between border-black">
+        <div className="flex flex-col">
+          <p className="text-9xl font-black capitalize text-white">
+            link at once!
+          </p>
+          <p className="text-4xl text-text-ddd">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <Link
+          href={ClientRoute.LOGIN}
+          className="button bg-white text-3xl text-primary shadow transition duration-300 hover:bg-primary-300 hover:text-primary-100 focus:bg-primary-300 focus:text-primary-100"
+        >
+          START!
+        </Link>
+      </div>
     </div>
   );
 }
