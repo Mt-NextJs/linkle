70c83ce4fc8b13860e66d7a63a41a161cd0fe2aa
an seungji <ansj8777@gmail.com>
2024-10-22 16:37:16 +0900

    ✨[Feat] 테스트를 위한 로그아웃, admin 이동 버튼으로 이루어진 임시 메인 페이지 구축 #32

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,38 @@
+"use client";
+
+import { ClientRoute } from "@config/route";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function Intro() {
+  const router = useRouter();
+
+  // 로그아웃 함수: 세션 스토리지와 쿠키에서 토큰 제거 후 리다이렉트
+  async function handleLogout() {
+    try {
+      // 인증 관련 데이터 제거
+      sessionStorage.removeItem("token");
+      document.cookie =
+        "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+      await router.push(ClientRoute.MAIN as string);
+      router.refresh();
+    } catch (error) {
+      console.error("로그아웃 중 오류 발생:", error);
+    }
+  }
+
   return (
     <>
       <h1>Home</h1>
       <h2>in my link</h2>
+      <Link href={ClientRoute.ADMIN as string} className="button">
+        Admin
+      </Link>
+
+      <button onClick={handleLogout} className="button">
+        logout
+      </button>
     </>
   );
 }
