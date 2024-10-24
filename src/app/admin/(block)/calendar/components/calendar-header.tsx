b09cf2fcb625cdf8f9c85307e7823fddab6cdf21
cchaeyoung <kimchaeyoung05@naver.com>
2024-10-24 12:48:14 +0900

    ♻️[Refactor] Layout 컴포넌트 활용을 위한 CalendarHeader 중복 요소 제거 #148

--- src/app/admin/(block)/calendar/components/calendar-header.tsx
+++ src/app/admin/(block)/calendar/components/calendar-header.tsx
@@ -1,7 +1,5 @@
 "use client";
-import Link from "next/link";
 import Image from "next/image";
-import { ClientRoute } from "@config/route";
 import { useRouter } from "next/navigation";
 
 export default function CalendarHeader() {
@@ -12,24 +10,15 @@ export default function CalendarHeader() {
   };
 
   return (
-    <div className="mt-4 flex w-full max-w-4xl flex-col p-8 pb-2">
-      <Link href={ClientRoute.MAIN} className="mb-6 ml-10 mr-4">
-        <Image
-          src="/assets/icons/icon_back.png"
-          alt="Back"
-          width={40}
-          height={40}
-        />
-      </Link>
-      <h1 className="mb-8 ml-10 mr-4 text-2xl font-semibold">캘린더 블록</h1>
-      <p className="ml-10 mr-4 text-gray-700">
+    <>
+      <p className="text-gray-700">
         진행/예정된 일정이 1개 이상이어야
         <br />
         캘린더 블록을 공개할 수 있습니다
       </p>
       <button
         onClick={handleAddScheduleClick}
-        className="button color ml-10 mr-10 flex w-auto flex-row items-center justify-center rounded-lg text-white"
+        className="button color flex w-auto flex-row items-center justify-center rounded-lg text-white"
       >
         <Image
           src="/assets/icons/icon_plus.png"
@@ -40,7 +29,7 @@ export default function CalendarHeader() {
         />
         <span>캘린더에 일정을 추가하세요</span>
       </button>
-      <hr className="border-gray-105 -mt-2 ml-8 mr-8 border-t-8" />
-    </div>
+      <hr className="border-gray-105 -mt-2 border-t-8" />
+    </>
   );
 }
