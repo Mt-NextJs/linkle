bdcdb678251b73c54502ca2c33f73426d7a97e67
cchaeyoung <kimchaeyoung05@naver.com>
2024-10-24 13:51:00 +0900

    🐛[Fix] form 이벤트 제어를 통한 토글 기능 버그 수정 #148

--- src/app/admin/(block)/calendar/page.tsx
+++ src/app/admin/(block)/calendar/page.tsx
@@ -6,13 +6,16 @@ import ScheduleList from "./components/schedule-list";
 import StyleSetting from "./components/style-setting";
 
 export default function CalendarPage() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    return false;
+  };
+
   return (
-    <>
-      <Layout title="캘린더 블록">
-        <CalendarHeader />
-        <StyleSetting />
-        <ScheduleList />
-      </Layout>
-    </>
+    <Layout title="캘린더 블록" onSubmit={handleSubmit}>
+      <CalendarHeader />
+      <StyleSetting />
+      <ScheduleList />
+    </Layout>
   );
 }
