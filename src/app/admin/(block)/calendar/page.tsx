e7acaa90829e20a8f12339388969d01ee9aa7740
cchaeyoung <kimchaeyoung05@naver.com>
2024-10-24 12:43:26 +0900

    ✨[Feat] CalendarPage에 공통 Layout 컴포넌트 적용 #148

--- src/app/admin/(block)/calendar/page.tsx
+++ src/app/admin/(block)/calendar/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Layout from "../components/layout";
 import CalendarHeader from "./components/calendar-header";
 import ScheduleList from "./components/schedule-list";
 import StyleSetting from "./components/style-setting";
@@ -7,11 +8,11 @@ import StyleSetting from "./components/style-setting";
 export default function CalendarPage() {
   return (
     <>
-      <div className="flex flex-col items-center">
+      <Layout title="캘린더 블록">
         <CalendarHeader />
         <StyleSetting />
         <ScheduleList />
-      </div>
+      </Layout>
     </>
   );
 }
