4c2e0afba40443e90c6acdea312b42f69a85bd0f
an seungji <ansj8777@gmail.com>
2024-10-29 14:19:27 +0900

    ✨[Feat] 유저 정보 조회 페이지 퍼블리싱 및 구현 #163

--- src/app/profile/detail/page.tsx
+++ src/app/profile/detail/page.tsx
@@ -3,6 +3,9 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { User } from "@/types/user";
+import FormInput from "@app/admin/(block)/components/form-input";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 export default function ProfileDetail() {
   const [userData, setUserData] = useState<User | null>(null);
@@ -36,17 +39,62 @@ export default function ProfileDetail() {
   if (!userData) return <p>Loading...</p>;
 
   return (
-    <div className="profile-detail">
-      <h1>회원 정보</h1>
-      <p>아이디: {userData.userId}</p>
-      <p>이름: {userData.name}</p>
-      <p>이메일: {userData.email || "미입력"}</p>
-      <button
-        className="edit-button"
-        onClick={() => router.push(`/profile/${userData.userId}/edit`)}
-      >
-        회원 정보 수정
-      </button>
+    <div className="flex min-h-screen flex-col justify-center gap-16 px-20 py-4">
+      <div className="sr-only">
+        <h1>IN MY LINK 회원 정보 페이지입니다!</h1>
+        <p>회원님의 정보를 확인하세요.</p>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <button type="button" onClick={() => router.back()}>
+          <Image
+            src="/assets/icons/icon_back.png"
+            alt="뒤로가기 아이콘"
+            width={34}
+            height={34}
+          />
+        </button>
+        <p className="pageName">IN MY LINK 회원 정보</p>
+      </div>
+
+      {/* 유저 정보 조회 폼 */}
+      <div className="flex w-full flex-col gap-4">
+        {/* 아이디 필드 */}
+        <FormInput
+          label="아이디"
+          id="userId"
+          value={userData.userId}
+          readOnly
+          className="border-gray-300 bg-gray-100"
+        />
+
+        {/* 이름 필드 */}
+        <FormInput
+          label="이름"
+          id="name"
+          value={userData.name}
+          readOnly
+          className="border-gray-300 bg-gray-100"
+        />
+
+        {/* 이메일 필드 */}
+        <FormInput
+          label="이메일"
+          id="email"
+          value={userData.email || "미입력"}
+          readOnly
+          className="border-gray-300 bg-gray-100"
+        />
+
+        {/* 회원 정보 수정 버튼 */}
+        <button
+          type="button"
+          className={twMerge("button color mt-16")}
+          onClick={() => router.push(`/profile/edit`)}
+        >
+          회원 정보 수정
+        </button>
+      </div>
     </div>
   );
 }
