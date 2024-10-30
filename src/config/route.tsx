74de4afca8381f67a833376b75e693f1eeaf67a0
an seungji <ansj8777@gmail.com>
2024-10-30 17:58:57 +0900

    ✨[Feat] 라우트 설정 파일에 유저 정보 조회, 수정 페이지 경로 수정 #180

--- src/config/route.tsx
+++ src/config/route.tsx
@@ -8,7 +8,7 @@ export const ClientRoute: ClientRouteType = {
   ADMIN: "/admin", // 관리자 페이지
   MY: "/admin/my", // 개인 페이지
   PROFILE: {
-    DETAIL: "/admin/profile/detail", // 프로필 상세 페이지
-    EDIT: "/admin/profile/edit", // 프로필 수정 페이지
+    DETAIL: "/profile/detail", // 프로필 상세 페이지
+    EDIT: "/profile/edit", // 프로필 수정 페이지
   },
 };
