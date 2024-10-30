bd572630878bbdf6220cacc441b39addbd035e16
an seungji <ansj8777@gmail.com>
2024-10-30 15:59:26 +0900

    🎨[Style] 초기 임시 메인 페이지에 배치했던 로그아웃 기능을 위한 주석 제거 #180

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,6 @@ import Admin from "@app/admin/page";
 export default function Page() {
   const router = useRouter();
 
-  // 로그아웃 함수: 세션 스토리지와 쿠키에서 토큰 제거 후 리다이렉트
-
   useEffect(() => {
     const token = sessionStorage.getItem("token");
     if (!token) {
