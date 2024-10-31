7845e73e593e881895213bd2ac734b0504f32bf9
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-10-31 10:04:12 +0900

    ✨ [Feat] basicblock하부컴포넌트(구분선) 이미지 적용 #14

--- src/app/admin/components/divide-block-sub/line.tsx
+++ src/app/admin/components/divide-block-sub/line.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
 export default function Line() {
   return (
-    <div className="relative ml-[85px] flex h-[86px] w-[530px] cursor-pointer items-center justify-center">
-      <div className="absolute left-0 top-1/2 w-full border-t-2 border-black"></div>
+    <div className="ml-[85px] flex h-[86px] w-[530px] items-center justify-center">
+      <Image
+        alt="line"
+        src="/assets/icons/SVG/item/item_line.svg"
+        width={500}
+        height={100}
+      />
     </div>
   );
 }
