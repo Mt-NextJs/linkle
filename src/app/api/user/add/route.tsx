34ae27622cf21fb175537d350a95c196e9497cbb
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-06 14:37:37 +0900

    ♻️ [Refactor] useradd,userinfo api 교체, 의존성 추가 #214

---
+++ src/app/api/user/add/route.tsx
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import clientPromise from "../../../../lib/mongodb";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { name, userId, password, email } = await request.json();
+
+    const client = await clientPromise;
+    const db = client.db("linkle");
+    const check = await db.collection("userdata").findOne({ name: name });
+    if (!check) {
+      await db.collection("userdata").insertOne({
+        name,
+        userId,
+        password,
+        email,
+        dateCreate: new Date(),
+      });
+      return NextResponse.json(
+        { message: "Sign up successfully" },
+        { status: 200 },
+      );
+    } else {
+      return NextResponse.json(
+        { message: "Name already exists" },
+        { status: 409 },
+      );
+    }
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+    return NextResponse.json(
+      { message: "Failed to create data", error: errorMessage },
+      { status: 500 },
+    );
+  }
+}
