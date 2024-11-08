81733cb0030e250ec14a8b44e4c94b60005a3726
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-08 14:49:47 +0900

    ♻️ [Refactor] profile/detail,profile/edit api 호출부 수정 #214

--- src/app/profile/detail/page.tsx
+++ src/app/profile/detail/page.tsx
@@ -6,7 +6,6 @@ import { User } from "@/types/user";
 import FormInput from "@app/admin/(block)/components/form-input";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
-import { getCookie } from "lib/get-cookie";
 
 export default function ProfileDetail() {
   const [userData, setUserData] = useState<User | null>(null);
@@ -15,18 +14,12 @@ export default function ProfileDetail() {
   useEffect(() => {
     async function fetchUserInfo() {
       try {
-        const token = getCookie("token"); // 쿠키에서 토큰 가져오기
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/api/user/info`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
-        );
+        const response = await fetch("/api/user/info", {
+          credentials: "include",
+        });
         const data = await response.json();
         if (response.ok) {
-          setUserData(data.data);
+          setUserData(data.user);
         } else {
           console.error("Failed to fetch user info:", data.message);
         }
@@ -34,10 +27,9 @@ export default function ProfileDetail() {
         console.error("Error fetching user info:", error);
       }
     }
-    fetchUserInfo();
+    fetchUserInfo().then();
   }, []);
-
-  if (!userData) return <p>Loading...</p>;
+  if (!userData) return <p>loading...</p>;
 
   return (
     <div className="flex min-h-screen flex-col justify-center gap-16 px-20 py-4">
