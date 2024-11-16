9ad1015d29bef0b53443a27d7ced2e028a6bd56b
an seungji <ansj8777@gmail.com>
2024-11-16 14:16:16 +0900

    🎨[Style] 프로필박스 임시로 배치했던 useTheme 제거 #216

--- src/app/admin/components/profile-box.tsx
+++ src/app/admin/components/profile-box.tsx
@@ -4,11 +4,9 @@ import HomeMenu from "@app/admin/components/home-menu";
 import Link from "next/link";
 import { ClientRoute } from "@config/route";
 import Image from "next/image";
-import { useTheme } from "@components/providers/theme-provider";
 import { usePathname } from "next/navigation";
 
 const ProfileBox = () => {
-  const { theme } = useTheme();
   const pathname = usePathname();
   const isAdmin = pathname === "/admin";
 
@@ -17,7 +15,7 @@ const ProfileBox = () => {
       <div
         className={twMerge(
           "relative mt-8 flex h-[200px] w-full flex-col items-center justify-center border text-center",
-          theme === "light" ? "bg-slate-100" : "bg-gray-800",
+          "border-[var(--input-color-line)] bg-[var(--background)] text-[var(--foreground)]",
         )}
         aria-labelledby="profile-name"
         role="region"
