c6e9705907e7db7d9f5aeb06632bfb9a670194eb
lumpen <swk5373@gmail.com>
2024-11-25 18:06:39 +0900

    ✨[Feat] 스케줄 추가 api 로직 변경 #273

--- src/app/admin/(block)/calendar/page.tsx
+++ src/app/admin/(block)/calendar/page.tsx
@@ -1,25 +1,46 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
+
+import { Schedule } from "@/types/user";
 
 import Layout from "../components/layout";
 import CalendarHeader from "./components/calendar-header";
 import ScheduleList from "./components/schedule-list";
 import StyleSetting from "./components/style-setting";
+import { adminApiInstance } from "../../../../utils/apis";
 
 function CalendarPage() {
+  const [schedules, setSchedules] = useState<Schedule[]>([]);
   const prevPath = useSearchParams().get("prevPath") || "/admin";
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     return false;
   };
 
+  const fetchSchedules = async () => {
+    const blockApis = await adminApiInstance;
+    const response = await blockApis.getSchedules();
+    if (!response) return;
+    if (response.ok) {
+      const {
+        result: { calendar },
+      } = await response.json();
+      console.log(calendar);
+      setSchedules(calendar as Schedule[]);
+    } else await blockApis.handleError(response);
+  };
+
+  useEffect(() => {
+    fetchSchedules().then();
+  }, []);
+
   return (
     <Layout title="캘린더 블록" onSubmit={handleSubmit} prevPath={prevPath}>
       <CalendarHeader />
-      <StyleSetting />
-      <ScheduleList />
+      <StyleSetting schedules={schedules} />
+      <ScheduleList schedules={schedules} />
     </Layout>
   );
 }
