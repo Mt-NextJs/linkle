39b2d85706b45a2c34483111fdd4365c14f6af01
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-11-27 12:22:09 +0900

    ✨ [Feat] api userlist   구현 #214

--- src/app/api/user/userlist/route.tsx
+++ src/app/api/user/userlist/route.tsx
@@ -7,9 +7,9 @@ export async function GET(request: NextRequest) {
     const client = await clientPromise;
     const db = client.db("linkle");
     const collection = db.collection("userdata");
-
     const limit = parseInt(request.nextUrl.searchParams.get("limit") || "10");
     const offset = parseInt(request.nextUrl.searchParams.get("offset") || "0");
+
     const users = await collection
       .find({})
       .project({ password: 0, _id: 0, dateCreate: 0, data: 0, calendar: 0 })
